bf8b1e6c19ad92e8b940063944c122cb192828fa
Amish <amish.tufail2002@gmail.com>
2024-07-04 12:02:31 +0500

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Amish on 04/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

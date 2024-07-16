10261cbbb7c3cc4e7420e4ce21f97c721d5cf34a
Amish <amish.tufail2002@gmail.com>
2024-07-16 14:43:00 +0500

    if applied, this will add map

--- DubDubGrub/LocationMapView.swift
+++ DubDubGrub/LocationMapView.swift
@@ -6,10 +6,24 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationMapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 37.331516,
+            longitude: -121.891054
+        ),
+        span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    )
     var body: some View {
-        Text("Map View")
+        ZStack(alignment: .top) {
+            Map(coordinateRegion: $region).ignoresSafeArea()
+            Image(.ddgMapLogo)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 70.0)
+        }
     }
 }
 
